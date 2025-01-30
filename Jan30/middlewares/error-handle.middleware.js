d9c037ef4df7014e0100b498cffad6d49dfd5d9b
akashkewat99 <akashkewat99@gmail.com>
2025-01-31 02:31:54 +0530

    modified in env

--- Jan30/middlewares/error-handle.middleware.js
+++ Jan30/middlewares/error-handle.middleware.js
@@ -1,7 +1,7 @@
 const errorMiddleware = (err, req, res, next) => {
   const status = err.status || 500;
-  const message = err.message || "BACKEND ERROR";
-  const extraDetails = err.extraDetails || "Error from Backend";
+  const message = "BACKEND ERROR" || err.message;
+  const extraDetails = "Error from Backend server" || err.extraDetails;
 
   return res.status(status).json({ status, message, extraDetails });
 };
